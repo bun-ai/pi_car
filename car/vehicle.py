7386ab32f5829ddb332ed92a8fd6056c401d88cf
bun_ai <bunalil@mail.ru>
2021-03-13 18:35:25 +0300

    Added PartProfiler.

--- car/vehicle.py
+++ car/vehicle.py
@@ -1,6 +1,50 @@
 import time
+import numpy as np
+
+from prettytable import PrettyTable
 
 from car.memory import Memory
+
+
+class PartProfiler:
+    def __init__(self):
+        self.records = {}
+
+    def profile_part(self, p):
+        self.records[p] = {"times": []}
+
+    def on_part_start(self, p):
+        self.records[p]['times'].append(time.time())
+
+    def on_part_finished(self, p):
+        now = time.time()
+        prev = self.records[p]['times'][-1]
+        delta = now - prev
+        thresh = 0.000001
+        if delta < thresh or delta > 100000.0:
+            delta = thresh
+        self.records[p]['times'][-1] = delta
+
+    def report(self):
+        print("Part Profile Summary: (times in ms)")
+        pt = PrettyTable()
+        field_names = ["part", "max", "min", "avg"]
+        pctile = [50, 90, 99, 99.9]
+        pt.field_names = field_names + [str(p) + '%' for p in pctile]
+        for p, val in self.records.items():
+            # remove first and last entry because you there could be one-off
+            # time spent in initialisations, and the latest diff could be
+            # incomplete because of user keyboard interrupt
+            arr = val['times'][1:-1]
+            if len(arr) == 0:
+                continue
+            row = [p.__class__.__name__,
+                   "%.2f" % (max(arr) * 1000),
+                   "%.2f" % (min(arr) * 1000),
+                   "%.2f" % (sum(arr) / len(arr) * 1000)]
+            row += ["%.2f" % (np.percentile(arr, p) * 1000) for p in pctile]
+            pt.add_row(row)
+        print(pt)
 
 
 class Vehicle:
@@ -25,6 +69,36 @@ class Vehicle:
         remove part form list
         """
         self.parts.remove(part)
+
+    def update_parts(self):
+        """
+        loop over all parts
+        """
+        for entry in self.parts:
+            run = True
+            # check run condition, if it exists
+            if entry.get('run_condition'):
+                run_condition = entry.get('run_condition')
+                run = self.mem.get([run_condition])[0]
+
+            if run:
+                # get part
+                p = entry['part']
+                # start timing part run
+                self.profiler.on_part_start(p)
+                # get inputs from memory
+                inputs = self.mem.get(entry['inputs'])
+                # run the part
+                if entry.get('thread'):
+                    outputs = p.run_threaded(*inputs)
+                else:
+                    outputs = p.run(*inputs)
+
+                # save the output to memory
+                if outputs is not None:
+                    self.mem.put(entry['outputs'], outputs)
+                # finish timing part run
+                self.profiler.on_part_finished(p)
 
     def start(self, rate_hz=10, max_loop_count=None, verbose=False):
         """
